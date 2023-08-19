6898c07c020d56406eb2741e495f0d0e7ef35ea9
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-19 14:27:34 +0300

    added a submenu for the account menu

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import './Navbar.scss'
 import { FiSearch } from 'react-icons/fi'
 import { FiGlobe } from 'react-icons/fi'
 import { FaUserCircle } from 'react-icons/fa'
-import {IoMenuOutline} from 'react-icons/io5'
+import { IoMenuOutline } from 'react-icons/io5'
+import { motion, AnimatePresence } from 'framer-motion'
 const Navbar = () => {
+    const [accountOpen, setAccountOpen] = useState(false)
+
+    const handleAccontOpen = () => {
+        setAccountOpen(!accountOpen)
+    }
+
+
     return (
         <nav className='navbar'>
             <div className='navbar__container containered'>
@@ -24,14 +32,26 @@ const Navbar = () => {
                 <div className="navbar__right">
                     <div className="nbr__container">
                         <div className="nbr__buttons">
-                            <button className='navbar__button'>Place Your Venue</button>
+                            <button className='navbar__button'>Venue Your Place</button>
                             <button className='navbar__button'>
                                 <FiGlobe className='navbar__button-icon' />
                             </button>
                         </div>
-                        <div className="nbr__account">
+                        <div className="nbr__account" onClick={handleAccontOpen}>
                             <IoMenuOutline className='navbar__account-icon' />
                             <FaUserCircle className='navbar__account-icon' />
+                            <AnimatePresence mode='wait'>
+                                {accountOpen && (
+                                    <motion.div initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.2 }} className="nbr__account-dropdown" >
+                                        <ul>
+                                            <li>Sign In</li>
+                                            <li>Sign Up</li>
+                                            <li>Venue Your Place</li>
+                                            <li>Help Center</li>
+                                        </ul>
+                                    </motion.div>
+                                )}
+                            </AnimatePresence>
                         </div>
                     </div>
                 </div>
