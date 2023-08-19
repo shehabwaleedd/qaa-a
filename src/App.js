404cdeacceb412d41c50ba47197f18e495dfc331
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-19 10:25:09 +0300

    finished navbar left and middle.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import { useLocation } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import ScrollUp from './components/scrollUp/ScrollUp';
+import Navbar from './navbar/Navbar';
+import Home from './pages/home/Home';
 export const ThemeContext = createContext(null);
 function App() {
   const location = useLocation();
 
   return (
     <div className="App">
+      <Navbar />
       <AnimatePresence mode='wait'>
         <Routes location={location} key={location.pathname} >
-          
+          <Route path='/' element={<Home />}/>
         </Routes>
       </AnimatePresence>
       <ScrollUp />
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import logo from '../assets/logo.png'
+import './Navbar.scss'
+import { FiSearch } from 'react-icons/fi'
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className='navbar'>
+            <div className='navbar__container'>
+                <div className="navbar__left">
+                    <img src={logo} alt="" />
+                    <h1>VENUES</h1>
+                </div>
+                <div className="navbar__middle">
+                    <span>Anywhere</span>
+                    <span>Anytime</span>
+                    <p>Anyplace</p>
+                    <div className="navbar__search">
+                        <FiSearch className='navbar__search-icon' />
+                    </div>
+                </div>
+                <div className="navbar__right">
+                    <button className='navbar__button'>Sign In</button>
+                    <button className='navbar__button'>Sign Up</button>
+                    <div className="nbr__account">
+                        <h1>Account</h1>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
 
